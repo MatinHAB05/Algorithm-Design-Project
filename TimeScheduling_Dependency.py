2300d46ffb5c19919a83d38004dde5e93e05809b
Matin Baki <m9652973@gmail.com>
2025-08-30 21:44:41 +0330

    add Schdeule Task With all dependencises and considration deadline

--- TimeScheduling_Dependency.py
+++ TimeScheduling_Dependency.py
@@ -1,5 +1,6 @@
 from MaxFlowMinCut import result as result_maxFlow
 from MaxFlowMinCut import mcf as graph
+
 assignment = result_maxFlow["assignment"]
 # print(assignment )
 
@@ -74,9 +75,56 @@ def find_ZerooutDegree_withMaxDeadline(node , Specfic_TopoGrap_temp) :
 
     return max(ZeroList,key=lambda t :t[1] )
 
+def Schedule_Tasks():
+    TaskComputedCount = len(assignment.keys())
+    j ={'schedule':{},'valid':False,'total_cost':result_maxFlow['total_cost']}
+    
+    while(TaskComputedCount>0) :
+        for node,TaskList in Schedule_Nodes.items() : 
+            if  len(TaskList)==0 : 
+                continue
+            print("****"+node+" "+TaskList[0][0])
+            if AttemptToRunTopTask(node,TaskList[0]):
+                TaskComputedCount-=1
+                del TaskList[0]
+                break
+    
+    for t,coll in task_node_sch_list.items() :
+        print(t+" : "+f"[start:{coll['start']},end:{coll['end']},duration:{coll['duration']},deadline:{coll['deadline']},flag:{coll['flag']}")
+        if coll['end']> coll['deadline'] :
+            print(f"DEADLINE!!!! === task : {t} , endTime : {coll['end'] } > deadline : {coll['deadline']}")
+            j['schedule']={}
+            return j
+    
+    for task,node in assignment.items():
+        j['schedule'][task]={'node':node,'start_time':task_node_sch_list[task]['start']}
+        
+    
+    
+    return j 
+
+def AttemptToRunTopTask(node,Task) : 
+    deadline_task=Task[1]
+    Task_id = Task[0]
+    #ParentCheck
+    max_ParentendTime=-97
+    for parent in dependencies_graph[Task_id]['parent'] :
+        if task_node_sch_list[parent]['flag']==0 :
+            return False
+        else :
+            max_ParentendTime=max(max_ParentendTime,task_node_sch_list[parent]['end'])
+    
+    if len(dependencies_graph[Task_id]['parent'] )==0 :
+        max_ParentendTime=0
+    #set StartTime
+    task_node_sch_list[Task_id]['start']=max_ParentendTime
+    task_node_sch_list[Task_id]['end']=max_ParentendTime+task_node_sch_list[Task_id]['duration']
+    task_node_sch_list[Task_id]['flag']=1
+    
+    return True
 
 input_dict = {
-"dependencies": [
+"dependencies": [ # assumption : valid Input[it is DAG !]
     {
     "before": "T1",
     "after": "T3"
@@ -94,13 +142,16 @@ input_dict = {
 "task_duration": {
     "T1": 1,
     "T2": 1,
-    "T3": 2 , 
-    "T4": 3 , 
+    "T3": 2, 
+    "T4": 3, 
     "T5": 3
  }
 }
 
 dependencies_graph , task_node_sch_list = CreateDependencies_Graph(input_dict["dependencies"],input_dict["task_duration"])
-# print(dependencies_graph)
+print(dependencies_graph)
 Schedule_Nodes = SchedulePerNode()
-print(Schedule_Nodes)
+print(Schedule_Nodes) 
+Schedule_Assigments_Tasks = Schedule_Tasks()
+# print(task_node_sch_list)
+print(Schedule_Assigments_Tasks)
